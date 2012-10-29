deaefc21dfda1c8c1c4554463ab2e5fd3211df80
Casey Maskalenko <caseyrm@gmail.com>
2012-10-29 16:26:37 -0400

    Added serial comm code for initialization and transmission.

--- AVR/avrcode.c
+++ AVR/avrcode.c
@@ -1,6 +1,12 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/iom88.h>
 #include <stdio.h>
+
+long getPower(void);
+void init_serial(void);
+void transmit_serial(unsigned char data);
+void transmit_message(char[255] message);
 
 int main(void) {
 	return 0;
@@ -24,3 +30,36 @@ long getPower()
 	return i*v;
 }
 
+void init_serial(void)
+{
+	//Set baud rate to 9600
+	UBRR0H = (unsigned char)(51>>8);
+	UBRR0L = (unsigned char) 51;
+
+	//Enable transmit/recieve
+	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
+
+	//Configure tansmission for 8 bit, 2 stop, no parity
+	UCSR0C = (3<<UCSZ00) | (1<<USBS0) | (0<<UPM00) | (0<<UPM01);
+}
+
+void transmit_message(char[255] message)
+{
+	int i;
+	
+	//Transmit the entire message char by char
+	for(i=0; i<256; i++)
+	{
+		transmit_serial(message[i]);
+	}
+	
+	//Ensure the last character is null-termination
+	transmit_serial('\0');
+}
+
+void transmit_serial(unsigned char data)
+{
+	//Wait for empty transmit buffer, then transmit
+	while(!(UCSR0A&(1<<UDRE0)));
+	UDR0 = data;
+}
