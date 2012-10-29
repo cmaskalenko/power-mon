4efa92bbc0c1537bca75ff88061524c45d6a1b7e
Casey Maskalenko <caseyrm@gmail.com>
2012-10-29 15:45:01 -0400

    Add serial intialization along with serial read code.

--- Client/client.c
+++ Client/client.c
@@ -1,38 +1,64 @@
 #include <stdio.c>
 #include <errno.h>
 #include <stderr.h>
+#include <termios.h>
 
-void init_serial(void);
+FILE* init_serial(void);
 string get_serial(void);
 char get_char(void);
 
 int main(int argc, char *argv[])
 {
-	FILE *writefile;
+	FILE* writefile, serialport;
 	char[256] sprint;
 
 	if (argc != 1)
 	{
-		printf("Incorrect number of arguments.\n");
+		printf("Incorrect number of arguments. Please only pass the address of an output file.\n");
 		exit(1);
 	}
 
+	//Open the file to write recieved data to
 	writefile = fopen(argv[1],"a");
-	if (writefile==null)
+	if (writefile == null)
 	{
-		fprintf("Failed to open output file");
+		perror("Failed to open output file");
 		return errno;
 	}
 
-	init_serial();
-
-	while(sprint!='\0')
+	//Intialize serial port
+	serialport = init_serial();
+	if (serialport == null)
 	{
-		sprint = get_serial();
+		perror("Failed to open serial communication");
+		return errno;
+	}
+
+	//Read from serial port, write to output file
+	while(sprint[0] != '\0')
+	{
+		fgets(sprint, sizeof(sprint), serialport);
 		fprintf(writefile, sprint);
 	}
 	
+	//Clean up
 	fclose(writefile);
+	fclose(serialport);
 
 	return 0;
+}
+
+FILE* init_serial()
+{
+	FILE* sport;
+	struct termios termc;
+
+	//Open the file handle for serial comm
+	sport = fopen("/dev/ttyACM0", "r+");
+
+	//Set baud rate
+	cfsetispeed(&termc, B9600);
+	cfsetospeed(&termc, B9600);
+
+	return sport;
 }
